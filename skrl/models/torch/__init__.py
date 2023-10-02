5ecb9be029de4cbff2a26aada7ecd3bd38952533
Christian Jenssen <cjenssen0@gmail.com>
2023-10-02 22:13:50 +0200

    Init `GaussianOptionsMixin` (clone of `GaussianMixin`)

--- skrl/models/torch/__init__.py
+++ skrl/models/torch/__init__.py
@@ -2,6 +2,6 @@ from skrl.models.torch.base import Model  # isort:skip
 
 from skrl.models.torch.categorical import CategoricalMixin
 from skrl.models.torch.deterministic import DeterministicMixin
-from skrl.models.torch.gaussian import GaussianMixin
+from skrl.models.torch.gaussian import GaussianMixin, GaussianOptionsMixin
 from skrl.models.torch.multivariate_gaussian import MultivariateGaussianMixin
 from skrl.models.torch.tabular import TabularMixin
